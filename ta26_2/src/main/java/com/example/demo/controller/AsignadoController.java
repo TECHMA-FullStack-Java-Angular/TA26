2d75edb0c4033b150f575ff54aae21a428630d88
Paula Sousa <paulasousa137@gmail.com>
2023-04-18 23:24:13 +0200

    prueba ej22

--- ta26_2/src/main/java/com/example/demo/controller/AsignadoController.java
+++ ta26_2/src/main/java/com/example/demo/controller/AsignadoController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.dto.Asignado;
-import com.example.demo.dto.Proyecto;
+
 import com.example.demo.service.AsignadoServiceImpl;
 
 @RestController
