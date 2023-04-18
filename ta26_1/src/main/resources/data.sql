4b4c9679a8aa35dce8acb522256ce46fedf8d87c
Paula Sousa <paulasousa137@gmail.com>
2023-04-18 17:40:05 +0200

    ej1 dto

--- ta26_1/src/main/resources/data.sql
+++ ta26_1/src/main/resources/data.sql
@@ -15,10 +15,11 @@ CREATE TABLE IF NOT EXISTS proveedores (
 );
 
 CREATE TABLE IF NOT EXISTS suministra (
+    id int(11) NOT NULL AUTO_INCREMENT,
     codigo_pieza INT,
     id_proveedor VARCHAR(4),
     precio INT,
-    CONSTRAINT PK_suministra PRIMARY KEY (codigo_pieza, id_proveedor),
+    CONSTRAINT PK_suministra PRIMARY KEY (id),
     CONSTRAINT FK_suministra_piezas FOREIGN KEY (codigo_pieza) REFERENCES piezas(codigo),
     CONSTRAINT FK_suministra_proveedores FOREIGN KEY (id_proveedor) REFERENCES proveedores(id)
 );
@@ -27,5 +28,5 @@ insert into piezas (codigo,nombre)values(1,'Pieza1');
 insert into piezas (codigo,nombre)values(2,'Pieza2');
 insert into proveedores (id,nombre)values('1A','Proveedor1');
 insert into proveedores (id,nombre)values('2B','Proveedor2');
-insert into suministra (codigo_pieza, id_proveedor, precio)values(1,'2B',200);
-insert into suministra (codigo_pieza, id_proveedor, precio)values(2,'1A',100);
+insert into suministra (id, codigo_pieza, id_proveedor, precio)values(1,1,'2B',200);
+insert into suministra (id, codigo_pieza, id_proveedor, precio)values(2,2,'1A',100);
